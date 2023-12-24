81b015955582424f6c3f70599b356a44a3c5d745
monnycerica <monnyc.e.t@gmail.com>
2023-12-24 16:29:48 +0800

    terjadi perubahan pada html dan css

--- js/app.js
+++ js/app.js
@@ -132,7 +132,7 @@ function init() {
 	displayAutoStartBreak();
 }
 
-//=========================Initialise Default/Local Storage values=================================================
+//Initialise Default/Local Storage values
 function displayTickSoundValue() {
 	if (localStorage.tickSoundInputValue === "true") {
 		tickSoundInput.checked = localStorage.tickSoundInputValue;
@@ -385,7 +385,7 @@ function titleTimeDisplay() {
 	titleDisplayText = allPossibleModes[currentTab].titleDisplayText;
 }
 
-//=================Notificiation, Ticking Sounds and Background Music=======================
+//Notificiation, Ticking Sounds and Background Music
 notificationTextInput.addEventListener("change", function () {
 	localStorage.notificationTextInputValue = notificationTextInput.value;
 });
@@ -426,7 +426,7 @@ function stopBackGroundMusic() {
 		allBackgroundMusic[allSounds].stop();
 	}
 }
-//===========Calculate percentage complete for progress bar================================
+//Calculate percentage complete for progress bar
 var degreeOfCircle;
 
 function progressDisplay() {
@@ -447,7 +447,7 @@ function progressDisplay() {
 	}
 }
 
-//=========================Minutes and Seconds converter==========================================
+//Minutes and Seconds converter
 function secondsToMinutes(s) {
 	var minutes = Math.floor(s / 60);
 	var seconds = s % 60;
@@ -464,7 +464,7 @@ function minutesToSeconds(m) {
 	var seconds = m * 60;
 	return seconds;
 }
-//====================================Dark and Light Modes======================================
+//Dark and Light Modes
 darkModeToggle.addEventListener("change", function () {
 	if (darkModeToggle.checked) {
 		darkMode();
@@ -520,7 +520,7 @@ function lightMode() {
 	$(".site-description").css("background", "#9ADBB3");
 	$(".form-control").css("background-color", "#efefef");
 }
-// ================================Get Time and Date=================================================
+//Get Time and Date
 function getDate() {
 	monthList = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	var today = new Date();
@@ -548,7 +548,7 @@ function getTime() {
 	var time = hours + ":" + minutes + amOrPm;
 	return time;
 }
-// ================================Adding date and time to log=================================================
+//Adding date and time to log
 function addDataToLog() {
 	var sessionsCol = document.createElement("th");
 	sessionsCol.setAttribute("scope", "row");
@@ -596,13 +596,13 @@ function addDataToLog() {
 	storeLogItems();
 	removeNoDataLoggedText();
 }
-// ================================Clear log===================================================
+//Clear log
 clearButton.addEventListener("click", function () {
 	locationUpdateLog.innerHTML = "";
 	storeLogItems();
 	showNoDataLoggedText();
 });
-// ===============================Delete log=================================================
+//Delete log
 function deleteLog(item) {
 	item.parentNode.parentNode.style.transition = "all 0.2s ease-in";
 	item.parentNode.parentNode.classList.add("slide-away");
@@ -614,7 +614,7 @@ function deleteLog(item) {
 		}
 	});
 }
-// ===========================Local Storage for Logging==============================================
+//Local Storage for Logging
 function storeLogItems() {
 	localStorage.logContents = locationUpdateLog.innerHTML;
 }
@@ -623,7 +623,7 @@ function storeLogDescription(item) {
 	item.outerHTML = '<td><input class="form-control" type="text" value="' + item.value + '" onchange="storeLogDescription(this)"></td>';
 	storeLogItems();
 }
-// =====================================No logging data text===============================================
+//No logging data text
 function showNoDataLoggedText() {
 	document.getElementById("NoDataLoggedText").style.display = "block";
 }
@@ -635,7 +635,7 @@ function removeNoDataLoggedText() {
 function logIsEmpty() {
 	return localStorage.logContents.indexOf("tr") === -1;
 }
-//==========================Todo list============================================================
+//Todo list
 var taskInput = document.getElementById("taskInput");
 taskInput.addEventListener("keypress", function () {
 	if (event.key === "Enter") {
@@ -704,11 +704,11 @@ function taskMouseOut(item) {
 	item.style.fontSize = "1rem";
 	item.style.transition = "100ms";
 }
-// ================================Local storage for todo list==========================================
+//Local storage for todo list
 function storeTasks() {
 	localStorage.todoContents = listOfTasks.innerHTML;
 }
-// ====================================No task Today Text====================================================
+//No task Today Text
 function showNoTaskTodayText() {
 	document.getElementById("NoTaskTodayText").style.display = "block";
 }
@@ -720,7 +720,7 @@ function removeNoTaskTodayText() {
 function listIsEmpty() {
 	return localStorage.todoContents.indexOf("li") === -1;
 }
-// ======================================Start Next Rounds===================================================
+//Start Next Rounds
 longBreakIntervalInput.addEventListener("input", function () {
 	localStorage.longBreakInterval = Number(longBreakIntervalInput.value);
 	localStorage.sliderValue = Number(longBreakIntervalInput.value);
@@ -779,7 +779,6 @@ function startNextRound() {
 		}
 	}
 }
-// ==================================Auto Start Next Round=====================================
 function autoStartTimer() {
 	if (timerRunning === false) {
 		timerRunning = true;
@@ -802,7 +801,7 @@ function buttonsDefaultState() {
 	stopButton.classList.remove("buttonClicked");
 	resetButton.classList.remove("buttonClicked");
 }
-// ============================Alerts==============================================================
+//Alerts
 var alert = document.querySelector(".alert");
 var alertMessage = document.querySelector("#alertMessage");
 
@@ -828,7 +827,7 @@ function showAlertMessage(session) {
 function dismissAlert() {
 	alert.style.display = "none";
 }
-// ===========================Scroll Indicator====================================================
+//Scroll Indicator
 window.addEventListener("scroll", moveScrollIndicator);
 const scrollIndicatorElt = document.getElementById("scrollIndicator");
 const maxHeight = window.document.body.scrollHeight - window.innerHeight;
@@ -837,7 +836,7 @@ function moveScrollIndicator(e) {
 	const percentage = (window.scrollY / maxHeight) * 100;
 	scrollIndicatorElt.style.width = percentage + "%";
 }
-// =====================Back to Top Button===========================================================
+//Back to Top Button
 window.addEventListener("scroll", displayScrollButton);
 
 function displayScrollButton() {
@@ -858,7 +857,7 @@ scrollButton.addEventListener("click", function () {
 		behavior: "smooth",
 	});
 });
-// ===================Input Validation==================================================
+//Input Validation
 $('input[type="number"]').attr(
 	"onkeypress",
 	"return (event.charCode == 8 || event.charCode == 0) ? null : event.charCode >= 46 && event.charCode <= 57"
